fcb1296b6885fe36dc384c79d4669bedc6ac7177
DemissenovDanial <demisenov.danial@mail.ru>
2024-01-23 02:06:02 +0600

    yes5

--- asdasd.js
+++ asdasd.js
@@ -9,6 +9,11 @@ let pressure = document.querySelector('.pressure');
 let wind = document.querySelector('.wind');
 let cod = document.querySelector('.cod');
 let coord = document.querySelector('.coord');
+let date = document.querySelector('.date');
+let title = document.querySelector('.title');
+let url = document.querySelector('.url');
+// let exp = document.querySelector('.exp');
+let news = document.querySelector('.news');
 
 button.addEventListener('click', function(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=108dd9a67c96f23039937fe6f3c91963`)
@@ -17,19 +22,38 @@ button.addEventListener('click', function(){
         displayData)
     .catch(err => alert('Wrong City name'));
     fetch(`https://api.nasa.gov/planetary/apod?api_key=O6oX0tGwcVPJCTtm74JCODQDPaxwupACXkee54sg`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Ошибка HTTP: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log("Дата:", data.date);
-    console.log("Заголовок:", data.title);
-    console.log("Изображение URL:", data.url);
-    console.log("Описание:", data.explanation);
-  })
-  .catch(error => console.error("Ошибка при получении данных:", error));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      date.innerText=`${"Дата: " + data.date}`
+      title.innerText=`${"Заголовок: " + data.title}`
+      url.innerHTML=`<img src="${data.url}" alt="NASA Image" style="width: 20%; height: 20%">`
+      // exp.innerText=`${"Описание:"+ data.explanation}`
+    })
+    .catch(error => console.error("Ошибка при получении данных:", error));
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=9a0a6d42d6524e02a488a22466b31ac8')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Ошибка запроса: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(info => {
+      if (info.articles) {
+        for (let i = 0; i < 3; i++) {
+          news.innerText += `News: ${info.articles[i].title},\n`;
+        }
+      } else {
+        console.error('Отсутствует поле "articles" в ответе API');
+      }
+    })
+    .catch(error => {
+      console.error(`Произошла ошибка: ${error.message}`);
+    });
 })
 
 let lon, lat;
